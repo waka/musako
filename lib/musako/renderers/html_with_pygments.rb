6339d33388bc91dc62716d9fce53292479224839
yo_waka <y.wakahara@gmail.com>
2014-05-10 03:13:22 +0900

    fix namespace

--- lib/musako/renderers/html_with_pygments.rb
+++ lib/musako/renderers/html_with_pygments.rb
@@ -5,7 +5,7 @@ module Musako
     # from https://github.com/vmg/redcarpet
     class HtmlWithPygments < Redcarpet::Render::HTML
       def block_code(code, language)
-        Pygments.highlight(code, :lexer => language)
+        ::Pygments.highlight(code, :lexer => language)
       end
     end
   end
